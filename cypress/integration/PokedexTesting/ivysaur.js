71a66d9a05bd098ea30fd1dd43bd3e039af724b6
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-08-07 22:16:35 +0300

    11.9 Simple end to end -tests

--- cypress/integration/PokedexTesting/ivysaur.js
+++ cypress/integration/PokedexTesting/ivysaur.js
@@ -4,4 +4,8 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Pokemon called Charmander can be opened and has right content', function() {
+    cy.get('#app').get('div').should('have.class', 'list-container').find('a').should('have.class', 'list-item').contains('charmander').click()
+    cy.get('#app').get('div').should('have.class', 'pokemon-name').contains('charmander').get('div').should('have.class', 'pokemon-ability-type').contains('Hidden ability')
+  })
 })
